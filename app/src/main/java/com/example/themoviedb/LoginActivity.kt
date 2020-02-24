b19c697fab60927e83a026c62224085a933c1854
sachinlimbu <sachinlimbu@outlook.com>
2020-02-24 16:40:53 +0000

    Pull Request Comments Resolved

--- app/src/main/java/com/example/themoviedb/LoginActivity.kt
+++ app/src/main/java/com/example/themoviedb/LoginActivity.kt
@@ -3,10 +3,10 @@ package com.example.themoviedb
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
