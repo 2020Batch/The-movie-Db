cf9032e6686d340127370840055d95b15f079d2d
sachinlimbu <sachinlimbu@outlook.com>
2020-02-24 13:47:25 +0000

    Feature/01-Splash Screen, Background color: Black, 2 sec delay timeout

--- app/src/main/java/com/example/themoviedb/SplashScreenActivity.kt
+++ app/src/main/java/com/example/themoviedb/SplashScreenActivity.kt
@@ -2,28 +2,23 @@ package com.example.themoviedb
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
-import java.lang.Exception
 
 class SplashScreenActivity : AppCompatActivity() {
+
+    private val SPLASH_TIME_OUT:Long=2000 //3 sec
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        val background = object : Thread() {
-            override fun run() {
-                try {
-                    sleep(3000)
-                    val intent = Intent(baseContext,MainActivity::class.java)
-                    startActivity(intent)
+        Handler().postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        },SPLASH_TIME_OUT)
 
-                }catch (e:Exception){
-                    e.printStackTrace()
-                }
-            }
-        }
-        background.start()
+
     }
 
 }
