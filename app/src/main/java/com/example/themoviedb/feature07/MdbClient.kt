8502ffd3c264d99ded0f59c7bb2b30b3377cbc8c
berryfurious <artyomvromanov@gmail.com>
2020-02-24 16:34:50 +0000

    messsage

---
+++ app/src/main/java/com/example/themoviedb/feature07/MdbClient.kt
@@ -0,0 +1,13 @@
+package com.example.themoviedb.feature07
+
+import com.example.themoviedb.feature07.model.PopularMovieModel
+import io.reactivex.Observable
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface MdbClient {
+
+    @GET(END_POINT_POPULAR_MOVIES)
+    fun getPopularMovieRepo(@Query("api_key") key: String?): Observable<PopularMovieModel>
+
+}
---
+++ app/src/main/java/com/example/themoviedb/feature07/PopularMoviesViewModelFactory.kt
@@ -0,0 +1,11 @@
+package com.example.themoviedb.feature07
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class PopularMoviesViewModelFactory(private val repo:MdbRepository):ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return PopularMoviesViewModel(repo) as T
+
+    }
+}
