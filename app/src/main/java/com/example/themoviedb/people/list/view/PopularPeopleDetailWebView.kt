4585c256d407989db73bb318cb87caf58ac9e975
sachinlimbu <sachinlimbu@outlook.com>
2020-02-25 17:20:57 +0000

    Web view for people detail, Updated Gradle Junit:4.12

---
+++ app/src/main/java/com/example/themoviedb/people/list/view/PopularPeopleDetailWebView.kt
@@ -0,0 +1,33 @@
+package com.example.themoviedb.people.list.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import com.example.themoviedb.R
+
+class PopularPeopleDetailWebView : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_popular_people_item_web_view)
+
+        val myWebView:WebView = findViewById(R.id.popular_people_item_web_view)
+        myWebView.webViewClient = object :WebViewClient(){
+            override fun shouldOverrideUrlLoading(
+                view: WebView?,
+                request: String?
+            ): Boolean {
+
+                view?.loadUrl(request)
+                return true
+            }
+        }
+
+        myWebView.loadUrl("https://www.carriefisher.com/")
+
+    }
+
+
+}
