4cb5b2685d2878dd8d6c31a3d1772c22710e92d7
lloyd <lloydhussell@gmail.com>
2020-02-25 17:04:16 +0000

    tests working

--- app/src/test/java/com/example/themoviedb/PopularMoviesTest.kt
+++ app/src/test/java/com/example/themoviedb/PopularMoviesTest.kt
@@ -1,14 +1,11 @@
 package com.example.themoviedb
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.lifecycle.Observer
 import com.example.themoviedb.common.network.model.Movie
 import com.example.themoviedb.common.network.model.PopularMovieModel
 import com.example.themoviedb.movies.list.model.MdbRepository
 import com.example.themoviedb.movies.list.model.MdbRepositoryImpl
-import com.example.themoviedb.movies.list.view.PopularMoviesActivity
 import com.example.themoviedb.movies.list.viewmodel.PopularMoviesViewModel
-import io.reactivex.Observable
 import io.reactivex.Single
 import org.junit.Assert
 import org.junit.Before
@@ -17,7 +14,6 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.Mockito.`when`
-import org.mockito.Mockito.verify
 import org.mockito.junit.MockitoJUnitRunner
 
 
@@ -41,7 +37,6 @@ class PopularMoviesTest {
 
     @Before
     fun setUp(){
-        repository = MdbRepositoryImpl()
         popMoviesViewModel = PopularMoviesViewModel(repository)
         movieModel = Movie(2.3,4,true,"dn23n23ed2.jpg", 2231,true,"ddowj2dojd2","ENGLISH","titleboo",
             listOf(),"titleboo2",2.3,"overvieeew","0123,12312")
