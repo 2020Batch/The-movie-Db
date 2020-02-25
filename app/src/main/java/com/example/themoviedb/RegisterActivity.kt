e818e20cc2cc1ba1e4bb66d4559de804ed127fca
berryfurious <artyomvromanov@gmail.com>
2020-02-25 16:19:25 +0000

    user messages bugs fixed

--- app/src/main/java/com/example/themoviedb/RegisterActivity.kt
+++ app/src/main/java/com/example/themoviedb/RegisterActivity.kt
@@ -11,7 +11,6 @@ import com.example.themoviedb.application.PASSWORD_LENGTH
 import com.example.themoviedb.loginregistervalidation.LoginRegViewModelFactory
 import com.example.themoviedb.loginregistervalidation.LoginRegistrationViewModel
 import com.example.themoviedb.loginregistervalidation.SharedPreferencesRepositoryImpl
-import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
@@ -20,7 +19,8 @@ class RegisterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
-        val model = ViewModelProvider(this, LoginRegViewModelFactory(SharedPreferencesRepositoryImpl(), application)
+        val model = ViewModelProvider(
+            this, LoginRegViewModelFactory(SharedPreferencesRepositoryImpl(), application)
         ).get(LoginRegistrationViewModel::class.java)
 
         btn_register.setOnClickListener {
@@ -28,15 +28,17 @@ class RegisterActivity : AppCompatActivity() {
             val username = et_reg_user_name.text.toString()
             val password = et_reg_password.text.toString()
 
-            if (password.length < PASSWORD_LENGTH) {
-
-                Toast.makeText(applicationContext, "Password should be at least $PASSWORD_LENGTH characters long",
-                    Toast.LENGTH_SHORT
-                ).show()
-
-            } else {
-
-                if (!username.isNullOrBlank() && !password.isNullOrBlank()) {
+            when {
+                username.trim().isBlank() -> {
+                    showToast(1, username)
+                }
+                password.trim().isBlank() -> {
+                    showToast(2, username)
+                }
+                password.length < PASSWORD_LENGTH -> {
+                    showToast(3, username)
+                }
+                else -> {
 
                     model.credentialsRegistration(username, password)
                     model.getRegistrationLiveData()
@@ -47,33 +49,74 @@ class RegisterActivity : AppCompatActivity() {
                             if (registered) {
 
                                 intent = Intent(this@RegisterActivity, LoginActivity::class.java)
-
-                                Toast.makeText(
-                                    applicationContext,
-                                    "${getString(R.string.txt_successful_registration)}", Toast.LENGTH_SHORT
-                                ).show()
-
+                                showToast(4, username)
                                 startActivity(intent)
 
                             } else {
 
-                                Toast.makeText(
-                                    applicationContext,
-                                    "${getString(R.string.txt_error_failed_registration)}", Toast.LENGTH_SHORT
-                                ).show()
+                                showToast(6, username)
 
                             }
 
                         })
-                } else {
-
-                    Toast.makeText(
-                        applicationContext,
-                        "${getString(R.string.error_empty_field)}", Toast.LENGTH_SHORT
-                    ).show()
-
                 }
             }
+
         }
     }
+
+    private fun showToast(type: Int, username : String) {
+
+        when (type) {
+
+            //Empty Username
+            1 -> Toast.makeText(
+                applicationContext, getString(R.string.txt_error_empty_username),
+                Toast.LENGTH_SHORT
+            ).show()
+
+            //Empty password
+            2 -> Toast.makeText(
+                applicationContext, getString(R.string.txt_error_empty_password),
+                Toast.LENGTH_SHORT
+            ).show()
+
+            //Password too short
+            3 -> Toast.makeText(
+                applicationContext,
+                "${getString(R.string.txt_error_password_too_short)} $PASSWORD_LENGTH ${getString(R.string.txt_error_characters)}",
+                Toast.LENGTH_SHORT
+            ).show()
+
+            //Registration success
+            4 -> Toast.makeText(
+                applicationContext,
+                "${getString(R.string.txt_successful_registration)} ${et_reg_user_name.text}",
+                Toast.LENGTH_SHORT
+            ).show()
+
+            //Login success
+            5 -> Toast.makeText(
+                applicationContext,
+                "${getString(R.string.txt_successful_login)} ${et_reg_user_name.text}",
+                Toast.LENGTH_SHORT
+            ).show()
+
+            //Registration failed
+            6 -> Toast.makeText(
+                applicationContext,
+                "${getString(R.string.txt_error_failed_registration)} ${et_reg_user_name.text}",
+                Toast.LENGTH_SHORT
+            ).show()
+
+            //Login failed
+            7 -> Toast.makeText(
+                applicationContext,
+                "${getString(R.string.txt_error_failed_login)} ${et_reg_user_name.text}",
+                Toast.LENGTH_SHORT
+            ).show()
+
+        }
+    }
+
 }
--- app/src/main/java/com/example/themoviedb/application/Constants.kt
+++ app/src/main/java/com/example/themoviedb/application/Constants.kt
@@ -3,4 +3,4 @@ package com.example.themoviedb.application
 const val PREFS_NAME = "PREFS_NAME"
 const val USERNAME_KEY = "USERNAME"
 const val PASSWORD_KEY = "PASSWORD"
-const val PASSWORD_LENGTH = 6;
+const val PASSWORD_LENGTH = 6
